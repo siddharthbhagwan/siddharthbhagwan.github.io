7a0d2f653f82392e3ef7d30322d69df9201e90c6
Siddharth Bhagwan <sid_sss@hotmail.com>
2014-11-25 23:05:54 +0530

    Index Draft 1

---
+++ js/application.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+
+//   $('.feature').hover(function(){
+//     if ($(this).hasClass('feature-border')){
+//       $(this).css('background-color', '#fafafa');
+//     }
+//   },
+//   function(){
+//     $(this).css('background-color', '#ffffff');
+//   });
+
+//   $('.feature').click(function(){
+//     // $('.feature').removeClass('feature-border');
+//     // $(this).addClass('feature-border');
+//     // $(this).css('background-color', '#ffffff');
+//     current_feature = $(this).parent().attr('id');
+//     $('.pane').hide();
+//     $('.pane-' + current_feature).show();
+//   });
+
+// });
