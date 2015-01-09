0f7e7b46d5b382eeea979442e80a56ec737ca072
Siddharth Bhagwan <sid_sss@hotmail.com>
2015-01-10 03:14:18 +0530

    Styling Fixes. Resume Updated.

--- js/application.js
+++ js/application.js
@@ -1,21 +1,25 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//   $('.feature').hover(function(){
-//     if ($(this).hasClass('feature-border')){
-//       $(this).css('background-color', '#fafafa');
-//     }
-//   },
-//   function(){
-//     $(this).css('background-color', '#ffffff');
-//   });
+  // $('.feature').hover(function(){
+  //   if ($(this).hasClass('feature-border')){
+  //     $(this).css('background-color', '#fafafa');
+  //   }
+  // },
+  // function(){
+  //   $(this).css('background-color', '#ffffff');
+  // });
 
-//   $('.feature').click(function(){
-//     // $('.feature').removeClass('feature-border');
-//     // $(this).addClass('feature-border');
-//     // $(this).css('background-color', '#ffffff');
-//     current_feature = $(this).parent().attr('id');
-//     $('.pane').hide();
-//     $('.pane-' + current_feature).show();
-//   });
+  // $('.feature').click(function(){
+  //   // $('.feature').removeClass('feature-border');
+  //   // $(this).addClass('feature-border');
+  //   // $(this).css('background-color', '#ffffff');
+  //   current_feature = $(this).parent().attr('id');
+  //   $('.pane').hide();
+  //   $('.pane-' + current_feature).show();
+  // });
 
-// });
+  $('.override-link').hover(function(){
+    $(this).css('text-decoration', 'none');
+  });
+
+});
